0af24ef99298318b521751754d506d9a7ab83572
kavitadsharma <kavitadsharma899107@gmail.com>
2022-12-07 22:04:50 +0530

    done

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 function identifyPrime(num) {
-    let count=2;
-    for(let i=1;i<===num;i++){
+    let count=0;
+    for(let i=1;i<=num;i++){
         if(num%i==0){
             count++;
         }
     }
-    if(count==1){
+    if(count==2){
         return true;
     }
         return false;
